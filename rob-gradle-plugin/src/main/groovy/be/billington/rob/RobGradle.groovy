a2122654550ac46077baec3497f743e9623d9bab
Benoit Billington <benoit@afrogleap.com>
2014-05-26 18:09:15 +0200

    More Stuff

--- rob-gradle-plugin/src/main/groovy/be/billington/rob/RobGradle.groovy
+++ rob-gradle-plugin/src/main/groovy/be/billington/rob/RobGradle.groovy
@@ -8,20 +8,34 @@ import org.gradle.api.tasks.StopExecutionException
 
 import be.billington.rob.bitbucket.RobLogBitbucketManager
 import be.billington.rob.github.RobLogGithubManager
+import be.billington.rob.RobLogManager
+import be.billington.rob.ConfigSections
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+
+import java.time.LocalDate
 
 
 class RobGradle implements Plugin<Project> {
     void apply(Project project) {
         Logger slf4jLogger = LoggerFactory.getLogger('some-logger')
         //StaticLoggerBinder.getSingleton().getLoggerFactory().getLogger("")
+
+        ConfigSections config = ConfigSections.createConfigSections(null, "");//prefix
+
+        RobLogManager manager
         if (true) {
-            manager = new RobLogBitbucketManager(slf4jLogger, config, key, secret, owner, repository, branch, startDate, endDate);
+            //manager = new RobLogBitbucketManager(slf4jLogger, config, key, secret, owner, repository, branch, startDate, endDate);
+            manager = new RobLogBitbucketManager(slf4jLogger, config, "", "", "afrogleap", "wecycle-android-recyclemanager", "development", LocalDate.now(), LocalDate.now());
         } else {
-            manager = new RobLogGithubManager(slf4jLogger, config, token, owner, repository, startDate, endDate);
+            //manager = new RobLogGithubManager(slf4jLogger, config, token, owner, repository, startDate, endDate);
+            manager = new RobLogGithubManager(slf4jLogger, config, "", "", "", LocalDate.now(), LocalDate.now());
         }
 
+
+        manager.fetchAndProcessCommitMessages();
+
+        manager.generateFile(targetDirectory, filePath);
 
     }
 }
