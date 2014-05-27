cd66ddf002e182662f54e012b7025e6eb25b7ba7
Benoit Billington <benoit@afrogleap.com>
2014-05-27 11:02:36 +0200

    Gradle Plugin: parameters

--- rob-gradle-plugin/src/main/groovy/be/billington/rob/RobGradle.groovy
+++ rob-gradle-plugin/src/main/groovy/be/billington/rob/RobGradle.groovy
@@ -2,40 +2,31 @@ package be.billington.rob
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.logging.Logger
-import org.gradle.api.logging.Logging
-import org.gradle.api.tasks.StopExecutionException
 
 import be.billington.rob.bitbucket.RobLogBitbucketManager
 import be.billington.rob.github.RobLogGithubManager
 import be.billington.rob.RobLogManager
 import be.billington.rob.ConfigSections
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 import java.time.LocalDate
 
 
 class RobGradle implements Plugin<Project> {
     void apply(Project project) {
-        Logger slf4jLogger = LoggerFactory.getLogger('some-logger')
-        //StaticLoggerBinder.getSingleton().getLoggerFactory().getLogger("")
 
-        ConfigSections config = ConfigSections.createConfigSections(null, "");//prefix
+        ConfigSections config = ConfigSections.createConfigSections(null, project.robPrefix)
 
         RobLogManager manager
-        if (true) {
-            //manager = new RobLogBitbucketManager(slf4jLogger, config, key, secret, owner, repository, branch, startDate, endDate);
-            manager = new RobLogBitbucketManager(slf4jLogger, config, "", "", "afrogleap", "wecycle-android-recyclemanager", "development", LocalDate.now(), LocalDate.now());
+        if (project.robApi.equals("bitbucket")) {
+            manager = new RobLogBitbucketManager(project.logger, config, project.bitbucketKey, project.bitbucketSecret, project.robOwner, project.robRepository, project.robBranch, LocalDate.now().minusDays(14), LocalDate.now())
         } else {
-            //manager = new RobLogGithubManager(slf4jLogger, config, token, owner, repository, startDate, endDate);
-            manager = new RobLogGithubManager(slf4jLogger, config, "", "", "", LocalDate.now(), LocalDate.now());
+            manager = new RobLogGithubManager(project.logger, config, project.githubToken, project.robOwner, project.robRepository, LocalDate.now().minusDays(14), LocalDate.now())
         }
 
 
-        manager.fetchAndProcessCommitMessages();
+        manager.fetchAndProcessCommitMessages()
 
-        manager.generateFile(targetDirectory, filePath);
+        manager.generateFile(null, project.robFile)
 
     }
 }
